d94c2f1602421d9b9b33b6514c7e18d547443a99
predrag jandric <predrag.jandric.bg@gmail.com>
2024-09-21 23:46:34 +0200

    will ADD: basic content and structure for Stats.jsx

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,10 +1,48 @@
+import { FaChartLine } from "react-icons/fa";
 
 const Stats = () => {
   return (
     <div>
-      Stats
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <h1>Over 40.00</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+            pariatur?
+          </p>
+          <div>
+            <div>
+              <div>
+                <span>
+                  <FaChartLine />
+                </span>
+                <h1>Data Validation</h1>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi itaque voluptatum fugit amet, quasi vel?
+              </p>
+            </div>
+            <div>
+              <div>
+                <span>
+                  <FaChartLine />
+                </span>
+                <h1>Optimise Efficiency</h1>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi itaque voluptatum fugit amet, quasi vel?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Stats
+export default Stats;
