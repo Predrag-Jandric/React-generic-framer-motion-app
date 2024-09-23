6446a1dcb1ab5be26d46f4556766173f64792377
predrag jandric <predrag.jandric.bg@gmail.com>
2024-09-23 23:47:06 +0200

    will ADD: basic content structure for Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,11 +1,58 @@
-
-
 const Testimonials = () => {
+  const testimonialsData = [
+    {
+      image:
+        "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg",
+      name: "Jon Doe",
+      position: "policeman",
+      feedback:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo, maiores. Ea similique beatae facere animi iste totam eligendi quidem sint?",
+    },
+    {
+      image:
+        "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg",
+      name: "Jon Doe",
+      position: "policeman",
+      feedback:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo, maiores. Ea similique beatae facere animi iste totam eligendi quidem sint?",
+    },
+    {
+      image:
+        "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg",
+      name: "Jon Doe",
+      position: "policeman",
+      feedback:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo, maiores. Ea similique beatae facere animi iste totam eligendi quidem sint?",
+    },
+  ];
+
   return (
     <div>
-      Testimonials
-    </div>
-  )
-}
+      <div>
+        <h1>Client Reviews</h1>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum saepe
+          debitis iure nesciunt dicta, magni facilis officiis optio impedit
+          exercitationem!
+        </h2>
 
-export default Testimonials
+        <div>
+          {testimonialsData.map((testimonial, index) => (
+            <div key={index}>
+              <div>
+                <img src={testimonial.image} alt="" />
+                <div>
+                  <h1>{testimonial.name}</h1>
+                  <p>{testimonial.position}</p>
+                </div>
+              </div>
+              <p>{testimonial.feedback}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
