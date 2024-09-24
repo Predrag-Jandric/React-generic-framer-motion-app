13e78f5bcb68dde9da63441886c703e85d7a3ccc
predrag jandric <predrag.jandric.bg@gmail.com>
2024-09-24 23:58:03 +0200

    will ADD: styling to Faq.jsx

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,3 +1,4 @@
+import { div } from "framer-motion/client";
 import { useState } from "react";
 
 const faqData = [
@@ -19,13 +20,28 @@ const faqData = [
 ];
 
 const Faq = () => {
-const [openFaq, setOpenFaq] = useState()
+  const [openFaq, setOpenFaq] = useState();
 
-const toggleFaq = (index) => {
-  setOpenFaq(openFaq === index ? null : index)
-}
+  const toggleFaq = (index) => {
+    setOpenFaq(openFaq === index ? null : index);
+  };
 
-  return <div></div>;
+  return (
+    <div className="w-4/5 m-auto my-20 p-4">
+      <h1 className="text-2xl font-semibold text-gray-600 mb-6">Frequesntly asked questions</h1>
+      {faqData.map((faq, index) => (
+        <div key={index} className="mb-4">
+          <div className="flex justify-between items-center bg-white/50 px-4 py-3 rounded-lg cursor-pointer">
+            <h2 className="text-lg font-medium text-gray-700">{faq.question}</h2>
+            <span className="text-xl text-gray-500">{openFaq === index ? "-" : "+"}</span>
+          </div>
+          <div className={`overflow-hidden transition-all duration-300 ${openFaq === index ? "max-h-40" : "max-h-0"}`} >
+            <p>{faq.answer}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Faq;
