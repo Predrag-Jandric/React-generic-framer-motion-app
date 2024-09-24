b9dcf2711cbc51934ae9ec68c185b2ea873fd6a1
predrag jandric <predrag.jandric.bg@gmail.com>
2024-09-25 01:30:22 +0200

    will ADD: unstyled content structure for Footer.jsx

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,4 +1,3 @@
-import { div } from "framer-motion/client";
 import { useState } from "react";
 
 const faqData = [
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,55 @@
-
-
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <footer>
+      <div>
+        <div>
+          <h2>About us</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, accusamus!</p>
+        </div>
+        <div>
+          <h2>Customer service</h2>
+          <ul>
+            <li>
+              <a href="#">Contact us</a>
+            </li>
+            <li>
+              <a href="#">Faqs</a>
+            </li>
+            <li>
+              <a href="#">Refunds</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2>Follow us</h2>
+          <ul>
+            <li>
+              <a href="#">Facebook</a>
+            </li>
+            <li>
+              <a href="#">Youtube</a>
+            </li>
+            <li>
+              <a href="#">Instagram</a>
+            </li>
+          </ul>
+        </div>
 
-export default Footer
+        <div>
+          <h3>Newsletter</h3>
+          <p>Enter your email to get latest news</p>
+          <form >
+            <input type="email" placeholder="your email adress" />
+            <button>Subscribe</button>
+          </form>
+        </div>
+      </div>
+
+      <div>
+        <p>Copyright {new Date().getFullYear()}</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
