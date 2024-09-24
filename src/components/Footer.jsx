4df6449061501fa67d7c273703e96cbc2e0eacc7
predrag jandric <predrag.jandric.bg@gmail.com>
2024-09-25 01:36:25 +0200

    will ADD: styling for Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,36 +1,36 @@
 const Footer = () => {
   return (
-    <footer>
-      <div>
+    <footer className="py-20">
+      <div className="w-4/5 m-auto grid md:grid-cols-4 gap-6 px-4">
         <div>
-          <h2>About us</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, accusamus!</p>
+          <h2 className="text-xl font-bold mb-2">About us</h2>
+          <p className="text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, accusamus!</p>
         </div>
         <div>
-          <h2>Customer service</h2>
+          <h2 className="font-bold mb-2">Customer service</h2>
           <ul>
             <li>
-              <a href="#">Contact us</a>
+              <a className="text-gray-400 hover:text-white" href="#">Contact us</a>
             </li>
             <li>
-              <a href="#">Faqs</a>
+              <a className="text-gray-400 hover:text-white" href="#">Faqs</a>
             </li>
             <li>
-              <a href="#">Refunds</a>
+              <a className="text-gray-400 hover:text-white" href="#">Refunds</a>
             </li>
           </ul>
         </div>
         <div>
-          <h2>Follow us</h2>
+          <h2 className="font-bold mb-2">Follow us</h2>
           <ul>
             <li>
-              <a href="#">Facebook</a>
+              <a className="text-gray-400 hover:text-white" href="#">Facebook</a>
             </li>
             <li>
-              <a href="#">Youtube</a>
+              <a className="text-gray-400 hover:text-white" href="#">Youtube</a>
             </li>
             <li>
-              <a href="#">Instagram</a>
+              <a className="text-gray-400 hover:text-white" href="#">Instagram</a>
             </li>
           </ul>
         </div>
@@ -38,14 +38,14 @@ const Footer = () => {
         <div>
           <h3>Newsletter</h3>
           <p>Enter your email to get latest news</p>
-          <form >
-            <input type="email" placeholder="your email adress" />
-            <button>Subscribe</button>
+          <form className="mt-4">
+            <input type="email" placeholder="your email adress" className="p-2 w-full rounded-md text-gray-600 border" />
+            <button className="mt-2 w-full p-2 bg-stone-800 rounded-md font-semibold text-white">Subscribe</button>
           </form>
         </div>
       </div>
 
-      <div>
+      <div className="text-center text-gray-500 mt-8 text-sm">
         <p>Copyright {new Date().getFullYear()}</p>
       </div>
     </footer>
